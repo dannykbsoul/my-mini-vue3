ad70742ed34da079fca7b23e0f9003883ae20f3d
moises.zhou <moises.zhou@nioint.com>
2022-03-19 23:15:58 +0800

    实现effect返回runer

--- src/reactivity/src/effective.ts
+++ src/reactivity/src/effective.ts
@@ -5,7 +5,7 @@ class ReactiveEffect {
   }
   run() {
     activeEffect = this;
-    this._fn();
+    return this._fn();
   }
 }
 
@@ -38,4 +38,5 @@ let activeEffect;
 export function effect(fn) {
   const _effect = new ReactiveEffect(fn);
   _effect.run();
+  return _effect.run.bind(_effect);
 }
--- src/reactivity/tests/effect.spec.ts
+++ src/reactivity/tests/effect.spec.ts
@@ -6,9 +6,20 @@ describe("reactivity/effect", () => {
     let dummy;
     const counter = reactive({ num: 0 });
     effect(() => (dummy = counter.num));
-
     expect(dummy).toBe(0);
     counter.num = 7;
     expect(dummy).toBe(7);
+  });
+
+  it("should return runner when call effect", () => {
+    let dummy = 10;
+    const runner = effect(() => {
+      dummy++;
+      return "foo";
+    })
+    expect(dummy).toBe(11);
+    const r = runner();
+    expect(dummy).toBe(12);
+    expect(r).toBe("foo");
   });
 });
