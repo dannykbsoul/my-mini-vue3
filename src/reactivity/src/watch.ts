3c72412b709d985e437d2d283ec755a6c837edec
moises.zhou <moises.zhou@nioint.com>
2022-03-27 22:18:44 +0800

    立即执行watch以及回调执行时机

--- src/reactivity/src/watch.ts
+++ src/reactivity/src/watch.ts
@@ -3,25 +3,43 @@ import { effect } from "./effective";
 
 // source 为 watch 的响应式对象 or 函数
 // 函数可以指定该 watch 依赖哪些响应式数据
-export function watch(source, cb) {
+export function watch(source, cb, options?: any) {
   let getter, oldValue, newValue;
   if (typeof source === "function") {
     getter = source;
   } else {
     getter = () => traverse(source);
   }
+
+  const job = () => {
+    // 在 scheduler 中重新执行 effect，得到 newValue
+    newValue = effectFn();
+    cb(newValue, oldValue);
+    // 更新 oldValue
+    oldValue = newValue;
+  };
   // 懒执行 这样第一次 effectFn() 拿到 oldValue
   const effectFn = effect(getter, {
     lazy: true,
-    scheduler() {
-      // 在 scheduler 中重新执行 effect，得到 newValue
-      newValue = effectFn();
-      cb(newValue, oldValue);
-      // 更新 oldValue
-      oldValue = newValue;
+    scheduler: () => {
+      // adjust the callback's flush timing
+      // 默认值 为 pre，if you attempt to access the DOM inside a watcher callback, the DOM will be in the state before Vue has applied any updates.
+      // flush 为 post，access the DOM in a watcher callback after Vue has updated it
+      if (options && options.flush === "post") {
+        const p = Promise.resolve();
+        p.then(job);
+      } else {
+        // 类似于 sync 的实现机制，pre 的方式暂时没办法模拟
+        job();
+      }
     },
   });
-  oldValue = effectFn();
+  // immdiate 立即触发回调函数
+  if (options && options.immdiate) {
+    job();
+  } else {
+    oldValue = effectFn();
+  }
 }
 
 // 对 source 递归的进行调用，即依赖收集，如果 source 有相关变化则会执行 cb
